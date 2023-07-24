3feca3831724efaac0e0a7a041612d89e04bbbc4
TexTrue <65154269+TexBlock@users.noreply.github.com>
2023-07-24 23:57:55 +0800

    Language datagen

--- src/main/java/ho/artisan/cobreacy/datagen/CobreacyDataGen.java
+++ src/main/java/ho/artisan/cobreacy/datagen/CobreacyDataGen.java
@@ -1,5 +1,8 @@
 package ho.artisan.cobreacy.datagen;
 
+import ho.artisan.cobreacy.Cobreacy;
+import net.minecraft.data.PackOutput;
+import net.minecraftforge.common.data.LanguageProvider;
 import net.minecraftforge.data.event.GatherDataEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -8,6 +11,32 @@ import net.minecraftforge.fml.common.Mod;
 public class CobreacyDataGen {
     @SubscribeEvent
     public static void dataGen(GatherDataEvent event) {
-        //event.getGenerator().addProvider();
+        var gen = event.getGenerator();
+        var packOutput = gen.getPackOutput();
+        var helper = event.getExistingFileHelper();
+        //gen.addProvider(event.includeClient(), new EnglishLanguageProvider(packOutput));
+        //gen.addProvider(event.includeClient(), new ChineseLanguageProvider(packOutput));
+    }
+
+    public static class EnglishLanguageProvider extends LanguageProvider {
+        public EnglishLanguageProvider(PackOutput output) {
+            super(output, Cobreacy.MODID, "en_us");
+        }
+
+        @Override
+        protected void addTranslations() {
+
+        }
+    }
+
+    public static class ChineseLanguageProvider extends LanguageProvider {
+        public ChineseLanguageProvider(PackOutput output) {
+            super(output, Cobreacy.MODID, "zh_cn");
+        }
+
+        @Override
+        protected void addTranslations() {
+
+        }
     }
 }
