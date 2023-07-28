87f9d5541102a6b2ab08d573c566243f299d0110
TexTrue <65154269+TexBlock@users.noreply.github.com>
2023-07-28 11:30:20 +0800

    FlowingCreblankFluid?

---
+++ src/main/java/ho/artisan/cobreacy/api/fluid/FlowingCreblankFluid.java
@@ -0,0 +1,21 @@
+package ho.artisan.cobreacy.api.fluid;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.LiquidBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.material.FlowingFluid;
+
+import java.util.function.Supplier;
+
+public class FlowingCreblankFluid extends LiquidBlock {
+    public FlowingCreblankFluid(Supplier<? extends FlowingFluid> supplier, Properties properties) {
+        super(supplier, properties);
+    }
+
+    @Override
+    public void entityInside(BlockState state, Level level, BlockPos pos, Entity entity) {
+
+    }
+}
--- src/main/java/ho/artisan/cobreacy/init/CBBlocks.java
+++ src/main/java/ho/artisan/cobreacy/init/CBBlocks.java
@@ -2,11 +2,13 @@ package ho.artisan.cobreacy.init;
 
 import ho.artisan.cobreacy.Cobreacy;
 import ho.artisan.cobreacy.api.block.MillingStoneBlock;
+import ho.artisan.cobreacy.api.fluid.FlowingCreblankFluid;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.LiquidBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.PushReaction;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.RegistryObject;
 
@@ -17,7 +19,7 @@ public class CBBlocks {
     public static final RegistryObject<Block> MILLING_STONE;
 
     static {
-        CREBLANK_FLUID_BLOCK = BLOCKS.register("creblank_fluid_block", () -> new LiquidBlock(CBFluids.CREBLANK_FLUID, BlockBehaviour.Properties.of().noLootTable().liquid().replaceable()));
+        CREBLANK_FLUID_BLOCK = BLOCKS.register("creblank_fluid_block", () -> new FlowingCreblankFluid(CBFluids.CREBLANK_FLUID, BlockBehaviour.Properties.of().noCollission().strength(100.0F).noLootTable().replaceable().liquid().noOcclusion().pushReaction(PushReaction.DESTROY)));
         MILLING_STONE = BLOCKS.register("milling_stone", () -> new MillingStoneBlock(BlockBehaviour.Properties.copy(Blocks.STONE)));
     }
 }
