35b0580a4b62cd27cc99d995c70d4d4e87b43565
TexTrue <65154269+TexBlock@users.noreply.github.com>
2023-07-30 19:49:40 +0800

    recipe (WIP)

--- src/main/java/ho/artisan/cobreacy/api/block/CreblankFluidBlock.java
+++ src/main/java/ho/artisan/cobreacy/api/block/CreblankFluidBlock.java
@@ -9,6 +9,6 @@ import net.minecraft.world.level.material.PushReaction;
 
 public class CreblankFluidBlock extends LiquidBlock {
     public CreblankFluidBlock() {
-        super(CBFluids.CREBLANK_FLUID, BlockBehaviour.Properties.of().mapColor(MapColor.WATER).replaceable().noCollission().strength(100.0F).pushReaction(PushReaction.DESTROY).noLootTable().liquid().sound(SoundType.EMPTY));
+        super(CBFluids.CREBLANK_FLUID, BlockBehaviour.Properties.of().mapColor(MapColor.COLOR_PURPLE).replaceable().noCollission().strength(100.0F).pushReaction(PushReaction.DESTROY).noLootTable().liquid().sound(SoundType.EMPTY));
     }
 }
